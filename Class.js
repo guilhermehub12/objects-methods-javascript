8582d37e57b6481a4eb1d514c1c1a4bff7e70d09
guilhermehub12 <guilhermee1312@hotmail.com>
2023-01-13 13:08:26 -0300

    class js

---
+++ Class.js
@@ -0,0 +1,19 @@
+// Class 
+class Microfone {
+    constructor(color = "black") {
+        this.color = color;
+        this.isOn = true;
+    }
+
+    toggleOnOff() {
+        if (this.isOn) {
+          console.log("desligar");
+        } else {
+          console.log("ligar");
+        }
+        this.isOn = !this.isOn;
+      }
+}
+
+const microfone = new Microfone("white");
+microfone.toggleOnOff();

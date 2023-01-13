813900eac82291dd9100c890b2c810a755a663f5
guilhermehub12 <guilhermee1312@hotmail.com>
2023-01-13 13:08:57 -0300

    objectLiteral method

---
+++ ObjectLiteral.js
@@ -0,0 +1,15 @@
+// Object Literal
+const microfone = {
+    color: "black",
+    isOn: true,
+    toggleOnOff: function () {
+        if (microfone.isOn) {
+            console.log("desligar");
+        } else {
+            console.log("ligar");
+        }
+        microfone.isOn = !microfone.isOn;
+    }
+}
+
+microfone.toggleOnOff();
